87e280203ea4d6066b9f250515a129c8f9dec5d8
Ayla Lopes <163359268+taengooni@users.noreply.github.com>
2024-08-23 22:48:10 +0000

    oiiii

---
+++ ex_02/javascript.js
@@ -0,0 +1,9 @@
+const btAdic = document.getElementById("btAdic");
+const inputNum = document.getElementById("inputNum");
+const inputSoma = document.getElementById("inputSoma");
+
+let somatorio = 0;
+
+btAdic.onclick = () = > {
+    let num = 
+}
