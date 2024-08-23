1ed2cc9d1d97f2106e20fc3d8ea882acef01f313
Ayla Lopes <163359268+taengooni@users.noreply.github.com>
2024-08-23 23:01:58 +0000

    oii

--- ex_02/javascript.js
+++ ex_02/javascript.js
@@ -1,9 +1,20 @@
 const btAdic = document.getElementById("btAdic");
 const inputNum = document.getElementById("inputNum");
 const inputSoma = document.getElementById("inputSoma");
-
 let somatorio = 0;
 
+
+
 btAdic.onclick = () = > {
-    let num = 
+    let num = parseInt(inputNum.value);
+    if (isNaN(num)) {
+        alert("Digite um número inteiro!");
+        return;
+    }
 }
+
+somatorio += num;
+inputSoma.value = somatorio;
+inputNum.value = "";
+inputNum.focus();
+};
