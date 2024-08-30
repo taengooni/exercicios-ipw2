117e801ef5f2125939b025e014975897e8b93157
Ayla Lopes <163359268+taengooni@users.noreply.github.com>
2024-08-30 22:04:51 +0000

    oii

--- ex_02/javascript.js
+++ ex_02/javascript.js
@@ -1,7 +1,10 @@
 const btAdic = document.getElementById("btAdic");
 const inputNum = document.getElementById("inputNum");
 const inputSoma = document.getElementById("inputSoma");
+const inputMedia = document.getElementById("inputMedia");
 let somatorio = 0;
+let count = 0;
+
 
 btAdic.onclick = () => {
   let num = parseInt(inputNum.value);
@@ -11,7 +14,10 @@ btAdic.onclick = () => {
   }
 
   somatorio += num;
+  count++;
+  let media = somatorio / count;
   inputSoma.value = somatorio;
+  inputMedia.value = media.toFixed(2);
   inputNum.value = "";
   inputNum.focus();
 };
